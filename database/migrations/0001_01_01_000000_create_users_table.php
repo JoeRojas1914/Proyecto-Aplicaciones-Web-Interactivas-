c3f780b22421d250d80155de4ece0b266570ab29
JoeRojas1914 <joerojas1914@gmail.com>
2024-11-27 18:20:40 -0600

    Correccion en migracion de usuario y adicion de middleware para admin

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('rol', ['usuario', 'administrador', 'critico'])->default('user');
+            $table->enum('rol', ['usuario', 'administrador', 'critico'])->default('usuario');
             $table->rememberToken();
             $table->timestamps();
         });
