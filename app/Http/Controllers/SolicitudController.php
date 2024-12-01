947e72b522a172b5f630b86985ee8c870ef594fb
JoeRojas1914 <joerojas1914@gmail.com>
2024-12-01 15:48:37 -0600

    Funcionalidad de Controlador de Solicitud

--- app/Http/Controllers/SolicitudController.php
+++ app/Http/Controllers/SolicitudController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Solicitud;
+use Illuminate\Support\Facades\Auth;
 
 class SolicitudController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'archivo' => 'required|file|mimes:pdf,jpeg,png|max:2048', 
+        ]);
+
+
+        $fileContent = file_get_contents($request->file('archivo')->getRealPath());
+
+        Solicitud::create([
+            'user_id' => Auth::id(), 
+            'file' => $fileContent, 
+            'status' => 'pending',
+        ]);
+
+        return redirect()->back()->with('status', 'profile-updated');
+    }
 }
